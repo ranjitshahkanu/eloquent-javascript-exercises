b08071ef881f993c6d45c3a8d38c4bb8b34a8e38
ranjitshahkanu <ranjitshahkanu.dev@gmail.com>
2026-05-23 18:56:23 +0545

    feat: add string and arithmetic  example

---
+++ exercises/01_values_types_operators/02_types/04_string_and_arithmetic.js
@@ -0,0 +1,6 @@
+import {benchmark} from "../../../src/common/timer.js";
+benchmark ("Arithmetic operation on string", () => {
+
+    //concatination of string using + operator 
+    console.log('C' + 'A' + "T");
+})
